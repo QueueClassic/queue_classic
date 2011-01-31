359df4027a396a419e1aa821a7d2dfe4f2768876
Ryan R. Smith <ryan@heroku.com>
2011-01-30 16:45:55 -0800

    added some qc scoped rake tasks

--- lib/queue_classic/tasks.rb
+++ lib/queue_classic/tasks.rb
@@ -1,5 +1,19 @@
+require 'database_helpers'
+
 namespace :jobs do
   task :work  => :environment do
     QC::Worker.new.start
+  end
+end
+namespace :qc do
+  task :create_table do
+    include DatabaseHelpers
+    create_table
+  end
+  task :work do
+    QC::Worker.new.start
+  end
+  task :jobs do
+    QC.queue_length
   end
 end
