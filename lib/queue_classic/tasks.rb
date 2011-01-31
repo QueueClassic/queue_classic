5972c8ff106c338a7aa8d29be7734be2385fb2ba
Ryan R. Smith <ryan@heroku.com>
2011-01-30 19:50:01 -0800

    removing db related tasks from task file. the user of this lib should know how to add a db table

--- lib/queue_classic/tasks.rb
+++ lib/queue_classic/tasks.rb
@@ -1,15 +1,9 @@
-require 'database_helpers'
-
 namespace :jobs do
   task :work  => :environment do
     QC::Worker.new.start
   end
 end
 namespace :qc do
-  task :create_table do
-    include DatabaseHelpers
-    create_table
-  end
   task :work do
     QC::Worker.new.start
   end
