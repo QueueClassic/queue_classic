b1ce2139c5bf5d75c38b3fefd836005bb1f4a47b
Ryan R. Smith <ryan@heroku.com>
2011-02-06 22:29:43 -0800

    added test helper to setup data_store

--- test/helper.rb
+++ test/helper.rb
@@ -1,8 +1,18 @@
 $: << File.expand_path("lib")
 $: << File.expand_path("test")
 
+ENV["DATABASE_URL"] = 'queue_classic_test'
+
 require 'queue_classic'
 require 'database_helpers'
 
 require 'minitest/unit'
 MiniTest::Unit.autorun
+
+def set_data_store(store=nil)
+  QC::Queue.instance.setup(
+    :data_store => (
+      store || QC::DurableArray.new(:database => ENV["DATABASE_URL"])
+    )
+  )
+end
