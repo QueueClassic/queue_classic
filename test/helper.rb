c240ee40708a5a476380daa5fe6119eb08531b8b
Ryan R. Smith <ryan@heroku.com>
2011-02-22 08:49:46 -0800

    added helpers for tests to make better syntax

--- test/helper.rb
+++ test/helper.rb
@@ -16,3 +16,19 @@ def set_data_store(store=nil)
     )
   )
 end
+
+def context(*args, &block)
+  return super unless (name = args.first) && block
+  klass = Class.new(MiniTest::Unit::TestCase) do
+    def self.test(name, &block)
+      define_method("test_#{name.gsub(/\W/,'_')}", &block) if block
+    end
+    def self.xtest(*args) end
+    def self.setup(&block) define_method(:setup, &block) end
+    def self.teardown(&block) define_method(:teardown, &block) end
+  end
+  (class << klass; self end).send(:define_method, :name) { name.gsub(/\W/,'_') }
+
+  klass.send :include, DatabaseHelpers
+  klass.class_eval &block
+end
