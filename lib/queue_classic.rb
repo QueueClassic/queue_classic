96060534d68bbe5dc3ce3b834e34982803fec7b6
Ryan R. Smith <ryan@heroku.com>
2011-03-27 21:57:31 -0700

    loading PG functions on init

--- lib/queue_classic.rb
+++ lib/queue_classic.rb
@@ -2,16 +2,21 @@ require 'json'
 require 'pg'
 require 'uri'
 
-$: << File.expand_path("lib")
+$: << File.expand_path(__FILE__, 'lib')
 
 require 'queue_classic/durable_array'
 require 'queue_classic/worker'
 require 'queue_classic/queue'
 require 'queue_classic/api'
 require 'queue_classic/job'
+require 'queue_classic/database_helpers'
 
 module QC
   extend Api
+  class Helper
+    extend DatabaseHelpers
+  end
 end
 
-QC::Queue.instance.setup :data_store => QC::DurableArray.new(ENV["DATABASE_URL"])
+QC::Helper.load_functions
+QC::Queue.instance.setup :data_store => QC::DurableArray.new(ENV['DATABASE_URL'])
