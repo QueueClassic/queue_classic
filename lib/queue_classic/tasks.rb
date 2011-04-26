da6b025e2fd0456db6288a3afca46ba00a2c212b
Ryan R. Smith <ryan@heroku.com>
2011-04-25 17:22:49 -0700

    rename function to convey meaning

--- lib/queue_classic/tasks.rb
+++ lib/queue_classic/tasks.rb
@@ -1,18 +1,30 @@
 namespace :jobs do
+
   task :work  => :environment do
     QC::Worker.new.start
   end
+
 end
 
 namespace :qc do
+
   task :work  => :environment do
     QC::Worker.new.start
   end
+
   task :jobs => :environment do
     QC.queue_length
   end
-  task :create_queue, :name :needs => :environment do |t,args|
+
+  task :load_functions => :environment do
+    db = QC::Database.new
+    db.load_functions
+    db.disconnect
+  end
+
+  task :create_queue, :name, :needs => :environment do |t,args|
     name = args[:name].to_sym
     QC::Database.create_queue(name)
   end
+
 end
