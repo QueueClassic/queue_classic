7a17c50cc2f2ba182fed372ae2a224005b583c9f
Ryan R. Smith <this.ryansmith@gmail.com>
2011-08-17 23:09:12 -0700

    syntax

--- lib/queue_classic/worker.rb
+++ lib/queue_classic/worker.rb
@@ -1,6 +1,6 @@
 module QC
   class Worker
-    MAX_LOCK_ATTEMPTS = ENV["QC_MAX_LOCK_ATTEMPTS"] ||= 5
+    MAX_LOCK_ATTEMPTS = ENV["QC_MAX_LOCK_ATTEMPTS"] || 5
 
     def initialize
       @running = true
