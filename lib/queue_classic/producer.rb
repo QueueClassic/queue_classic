defdab890acd76fb76c168d01310966b096b7de3
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-01-20 14:30:38 -0700

    Update Producer documentation

--- lib/queue_classic/producer.rb
+++ lib/queue_classic/producer.rb
@@ -3,6 +3,12 @@ module QueueClassic
   # A Producer puts items on to the Queue.
   #
   class Producer
+    # Create a new producer
+    #
+    # session    - the Session object this producer is attached
+    # queue_name - the name of the queue to put items onto
+    #
+    # Returns the new producer object
     def initialize( session, queue_name )
       @session = session
       @queue   = session.use_queue( queue_name )
