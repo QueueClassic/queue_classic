8bcbda86cac89ec3f06c1cd0b63c51972f00862c
Jeremy Hinegardner <jeremy@hinegardner.org>
2012-01-20 21:53:39 -0700

    Consumers get an id

--- lib/queue_classic/consumer.rb
+++ lib/queue_classic/consumer.rb
@@ -3,6 +3,12 @@ module QueueClassic
   # A Consumer pulls items off of the Queue.
   #
   class Consumer
+    # The queue the consumer is attached to
+    attr_reader :queue
+
+    # The unique identifier of the consumer as determined by the connection
+    attr_reader :consumer_id
+
     # Create a new consumer
     #
     # session    - the Session object this Consumer is attached
@@ -12,6 +18,7 @@ module QueueClassic
     def initialize( session, queue_name )
       @session    = session
       @queue      = session.use_queue( queue_name )
+      @consumer_id = connection.apply_application_name( 'consumer' )
     end
 
     #######
