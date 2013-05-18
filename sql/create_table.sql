1afe4a36760f573c6d06471794aa87a772f9eb97
Joe Van Dyk <joe@fixieconsulting.com>
2013-05-18 11:42:17 -0700

    Use json type for the args column if json is available.
    Also add some constraints to the q_name and method columns.

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -1,9 +1,19 @@
+do $$ begin
+
 CREATE TABLE queue_classic_jobs (
   id bigserial PRIMARY KEY,
-  q_name varchar(255),
-  method varchar(255),
-  args text,
+  q_name text not null check (length(q_name) > 0),
+  method text not null check (length(method) > 0),
+  args   text not null,
   locked_at timestamptz
 );
+
+-- If json type is available, use it for the args column.
+perform * from pg_type where typname = 'json';
+if found then
+  alter table queue_classic_jobs alter column args type json using (args::json);
+end if;
+
+end $$ language plpgsql;
 
 CREATE INDEX idx_qc_on_name_only_unlocked ON queue_classic_jobs (q_name, id) WHERE locked_at IS NULL;
