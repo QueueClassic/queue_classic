5db362493aa158d39f7612b24e288e60f57cf3cb
Ryan Smith <ryan@heroku.com>
2013-06-15 18:02:57 -0700

    enable other benchmarks

--- test/benchmark_test.rb
+++ test/benchmark_test.rb
@@ -32,4 +32,33 @@ class BenchmarkTest < QCTest
     assert_equal(0, QC.count)
     assert_in_delta(10, elapsed, 3)
   end
+
+  def test_concurrent_dequeue
+    Thread.abort_on_exception = true
+
+    queue = QC::Queue.new
+    num_threads = 8
+    n = 10_000
+
+    n.times do
+      queue.enqueue("1.odd?", [])
+    end
+    assert_equal(n, queue.count)
+
+    start = Time.now
+    num_threads.times.map do
+      Thread.new do
+        worker = QC::Worker.new(queue)
+        worker.running = true
+        (n / num_threads).times do
+          worker.work(num_threads)
+        end
+      end
+    end.map(&:join)
+    assert_equal(0, queue.count)
+
+    elapsed = Time.now - start
+    assert_in_delta(5, elapsed, 3)
+  end
+
 end
