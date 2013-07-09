fcb4d848a990e84d780ee874b538e279a4ad3bd7
Ryan Smith <r@32k.io>
2013-07-09 10:42:14 -0700

    improve logging in queue class

--- lib/queue_classic/queue.rb
+++ lib/queue_classic/queue.rb
@@ -8,37 +8,42 @@ module QC
     end
 
     def enqueue(method, *args)
-      QC.log_yield(:action => "insert_job") do
+      QC.log_yield(:measure => 'queue.enqueue') do
         s="INSERT INTO #{TABLE_NAME} (q_name, method, args) VALUES ($1, $2, $3)"
         res = Conn.execute(s, name, method, JSON.dump(args))
       end
     end
 
     def lock
-      s = "SELECT * FROM lock_head($1, $2)"
-      if r = Conn.execute(s, name, top_bound)
-        {
-          :id     => r["id"],
-          :method => r["method"],
-          :args   => JSON.parse(r["args"])
-        }
+      QC.log_yield(:measure => 'queue.lock') do
+        s = "SELECT * FROM lock_head($1, $2)"
+        if r = Conn.execute(s, name, top_bound)
+          {:id => r["id"],
+            :method => r["method"],
+            :args => JSON.parse(r["args"])}
+        end
       end
-
     end
 
     def delete(id)
-      Conn.execute("DELETE FROM #{TABLE_NAME} where id = $1", id)
+      QC.log_yield(:measure => 'queue.delete') do
+        Conn.execute("DELETE FROM #{TABLE_NAME} where id = $1", id)
+      end
     end
 
     def delete_all
-      s = "DELETE FROM #{TABLE_NAME} WHERE q_name = $1"
-      Conn.execute(s, name)
+      QC.log_yield(:measure => 'queue.delete_all') do
+        s = "DELETE FROM #{TABLE_NAME} WHERE q_name = $1"
+        Conn.execute(s, name)
+      end
     end
 
     def count
-      s = "SELECT COUNT(*) FROM #{TABLE_NAME} WHERE q_name = $1"
-      r = Conn.execute(s, name)
-      r["count"].to_i
+      QC.log_yield(:measure => 'queue.count') do
+        s = "SELECT COUNT(*) FROM #{TABLE_NAME} WHERE q_name = $1"
+        r = Conn.execute(s, name)
+        r["count"].to_i
+      end
     end
 
   end
