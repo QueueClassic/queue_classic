8794dbce3566d45d3cace8e917e97a63764bffc9
Ryan Smith <r@32k.io>
2013-07-09 10:47:15 -0700

    move helper sql into file

--- test/helper.rb
+++ test/helper.rb
@@ -21,32 +21,7 @@ class QCTest < Minitest::Test
     QC::Conn.execute("SET client_min_messages TO 'warning'")
     QC::Setup.drop
     QC::Setup.create
-    QC::Conn.execute(<<EOS)
-DO $$
--- Set initial sequence to a large number to test the entire toolchain
--- works on integers with higher bits set.
-DECLARE
-    quoted_name text;
-    quoted_size text;
-BEGIN
-    -- Find the name of the relevant sequence.
-    --
-    -- pg_get_serial_sequence quotes identifiers as part of its
-    -- behavior.
-    SELECT name
-    INTO STRICT quoted_name
-    FROM pg_get_serial_sequence('queue_classic_jobs', 'id') AS name;
-
-    -- Don't quote, because ALTER SEQUENCE RESTART doesn't like
-    -- general literals, only unquoted numeric literals.
-    SELECT pow(2, 34)::text AS size
-    INTO STRICT quoted_size;
-
-    EXECUTE 'ALTER SEQUENCE ' || quoted_name ||
-        ' RESTART ' || quoted_size || ';';
-END;
-$$;
-EOS
+    QC::Conn.execute(File.read('./test/helper.sql'))
   end
 
   def capture_debug_output
