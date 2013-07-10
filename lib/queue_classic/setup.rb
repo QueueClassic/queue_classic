ce17f2b9d19b536dce51ef1ff2e883823bc7f2cc
Ryan Smith <r@32k.io>
2013-07-09 22:00:30 -0700

    ensure we drop funcs

--- lib/queue_classic/setup.rb
+++ lib/queue_classic/setup.rb
@@ -3,16 +3,16 @@ module QC
     Root = File.expand_path("../..", File.dirname(__FILE__))
     SqlFunctions = File.join(Root, "/sql/ddl.sql")
     CreateTable = File.join(Root, "/sql/create_table.sql")
+    DropSqlFunctions = File.join(QC::Root, "/sql/drop_ddl.sql")
 
     def self.create
-      Conn.transaction do
-        Conn.execute(File.read(CreateTable))
-        Conn.execute(File.read(SqlFunctions))
-      end
+      Conn.execute(File.read(CreateTable))
+      Conn.execute(File.read(SqlFunctions))
     end
 
     def self.drop
       Conn.execute("DROP TABLE IF EXISTS queue_classic_jobs CASCADE")
+      Conn.execute(File.read(DropSqlFunctions))
     end
   end
 end
