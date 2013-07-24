13fe4ea6aa6074812d1de73e9c9a8c3f131eda6f
Ryan Smith <r@32k.io>
2013-07-24 11:18:02 -0700

    improve docs around conn pool

--- lib/queue_classic/pool.rb
+++ lib/queue_classic/pool.rb
@@ -2,15 +2,21 @@ require 'queue_classic/conn'
 require 'thread'
 
 module QC
+  # A thread safe container for accessing database connections.
   class Pool
 
     attr_accessor :conns
+    # Creating a new pool will
+    # connect all connections in the pool to PostgreSQL.
     def initialize(sz=1)
       @mutex = Mutex.new
       @conns = SizedQueue.new(sz)
       sz.times {@conns.enq(Conn.new)}
     end
 
+    # This method is the primary way to use a connection in
+    # the pool. It will take care of checking out the connection
+    # and returning the connection to the pool.
     def use(new_on_empty=true)
       raise("Expected a block with Pool#use.") unless block_given?
       c = checkout(new_on_empty)
@@ -23,6 +29,8 @@ module QC
       return result
     end
 
+    # Remove all connections from the pool
+    # while disconnecting them from the server.
     def drain!
       until conns.size.zero?
         c = conns.deq
@@ -45,6 +53,8 @@ module QC
     end
 
     def checkin(c)
+      # We synchronize this function
+      # because we do a comparison followed by an enqueue.
       @mutex.synchronize do
         if conns.size == conns.max
           c.disconnect
