6d5ad42f173727ca0c1c969b9f4332ca24e9f3a3
Ryan Smith <r@32k.io>
2014-01-05 13:41:17 -0800

    Refactor: remove unused methods
    This lib does not use these methods. If a client is calling these
    methods, that code should be removed.

--- lib/queue_classic/conn.rb
+++ lib/queue_classic/conn.rb
@@ -33,21 +33,6 @@ module QC
       drain_notify
     end
 
-    def transaction
-      begin
-        execute("BEGIN")
-        yield
-        execute("COMMIT")
-      rescue Exception
-        execute("ROLLBACK")
-        raise
-      end
-    end
-
-    def transaction_idle?
-      connection.transaction_status == PGconn::PQTRANS_IDLE
-    end
-
     def connection
       @connection ||= connect
     end
