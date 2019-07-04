cb12ded793e68814e9150d78b95abc045951c7c7
Russell Smith <ukd1@users.noreply.github.com>
2019-07-03 18:05:32 -0700

    Optimzie a little

--- test/rails-tests/rails523.sh
+++ test/rails-tests/rails523.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-export BUNDLE_GEMFILE=Gemfile523
-bundle check --path=vendor/bundle || bundle install --path=vendor/bundle --jobs=4 --retry=3
-
 rm -rf qctest523
 bundle exec rails new qctest523 --api --database=postgresql --skip-test-unit --skip-keeps --skip-spring --skip-sprockets --skip-javascript --skip-turbolinks
 
