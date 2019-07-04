b0bd6145ae5b86ad377fb7276a82f0f26474d0dd
Russell Smith <ukd1@users.noreply.github.com>
2019-07-03 19:45:30 -0700

    fixes

--- test/rails-tests/rails523.sh
+++ test/rails-tests/rails523.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 set -e
+export BUNDLE_GEMFILE=Gemfile523
 
 rm -rf qctest523
 bundle exec rails new qctest523 --api --database=postgresql --skip-test-unit --skip-keeps --skip-spring --skip-sprockets --skip-javascript --skip-turbolinks
 
+unset BUNDLE_GEMFILE
+
 cd qctest523
 
-rails db:drop:all
-rails db:create
-rails db:migrate
-rails db:setup
+bundle exec rails db:drop:all
+bundle exec rails db:create
+bundle exec rails db:migrate
+bundle exec rails db:setup
 
 echo "gem 'queue_classic', path: '../../../'" >> Gemfile
 
-rails generate queue_classic:install
-bundle exec rake db:migrate
+bundle exec rails generate queue_classic:install
+bundle exec rails db:migrate
