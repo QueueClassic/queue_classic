090a6773c8d7ca92e55df201d6839f3da3b8ca4f
Russell Smith <ukd1@users.noreply.github.com>
2019-07-18 12:34:20 -0700

    Test

--- test/rails-tests/rails523.sh
+++ test/rails-tests/rails523.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 set -e
 
+# remove any old folder, should only matter locally
 rm -rf qctest523
+
+# install rails but not with much stuff
 gem install rails -v 5.2.3
 rails new qctest523 --api --database=postgresql --skip-test-unit --skip-keeps --skip-spring --skip-sprockets --skip-javascript --skip-turbolinks
-
-echo "1"
-
 cd qctest523
 
+# get the db setup, run any default migrations
+gem install rake -v 12.3.2
 bundle exec rails db:drop:all
 bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:setup
 
-echo "2"
-
+# install qc --> gem file, bundle, add ourselves and migrate.
 echo "gem 'queue_classic', path: '../../../'" >> Gemfile
 bundle install
-
-echo "3"
 bundle exec rails generate queue_classic:install
 bundle exec rails db:migrate
